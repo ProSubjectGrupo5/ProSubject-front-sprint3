d12e5161137a81718c67a0ee1f3d7a31371e9733
crilorbre <43986572+crilorbre@users.noreply.github.com>
2020-03-20 17:20:40 +0100

    cambios en registro

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -30,7 +30,8 @@ export class RegistroComponent implements OnInit {
   ngOnInit() {
     this.form = this.fb.group({
       nombre: new FormControl('', [Validators.required]),
-      apellidos: new FormControl('', [Validators.required]),
+      apellido1: new FormControl('', [Validators.required]),
+      apellido2: new FormControl('', [Validators.required]),
       dni: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{8}[A-Z]{1}$')]),
       email: new FormControl('', [Validators.required, Validators.email]),
       telefono: new FormControl('', [Validators.required]),
@@ -44,7 +45,20 @@ export class RegistroComponent implements OnInit {
   }
 
   onSubmit(){
-    console.log(this.form.value)
+
+    //crear usuario
+    this.usuario.nombre = this.form.get('nombre').value;
+    this.usuario.apellido1 = this.form.get('apellido1').value;
+    this.usuario.apellido2 = this.form.get('apellido2').value;
+    this.usuario.dni = this.form.get('dni').value;
+    this.usuario.email = this.form.get('email').value;
+    this.usuario.telefono = this.form.get('telefono').value;
+    this.usuario.useraccount.username = this.form.get('useraccount').get('username').value;
+    this.usuario.useraccount.password = this.form.get('useraccount').get('password').value;
+    this.usuario.useraccount.autoridad = this.form.get('useraccount').get('autoridad').value;
+    
+    console.log(this.usuario);
+
   }
 
 }
