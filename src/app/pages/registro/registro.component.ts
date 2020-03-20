7c2338675092a78340ded9ef7c49cae83d7433f3
crilorbre <43986572+crilorbre@users.noreply.github.com>
2020-03-20 11:04:54 +0100

    añadido validacion de confirmar contraseña

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { confirmPasswordValidator } from "./confirm-password-validator";
 
 @Component({
   selector: 'app-registro',
@@ -11,21 +11,35 @@ export class RegistroComponent implements OnInit {
 
   form:FormGroup;
 
+  usuario: any = {
+    nombre:'',
+    apellido1:'',
+    apellido2:'',
+    dni:'',
+    email:'',
+    telefono:'',
+    useraccount: {
+      username:'',
+      password:'',
+      autoridad:''
+    }
+  }
+
   constructor(private fb:FormBuilder) { }
 
   ngOnInit() {
     this.form = this.fb.group({
-      nombre: new FormControl(''),
-      apellidos: new FormControl(''),
-      dni: new FormControl(''),
-      email: new FormControl(''),
-      telefono: new FormControl(''),
+      nombre: new FormControl('', [Validators.required]),
+      apellidos: new FormControl('', [Validators.required]),
+      dni: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{8}[A-Z]{1}$')]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      telefono: new FormControl('', [Validators.required]),
       useraccount: new FormGroup({
-        username: new FormControl(''),
-        password: new FormControl(''),
+        username: new FormControl('', [Validators.required]),
+        password: new FormControl('', [Validators.required]),
         confirmPassword: new FormControl(''),
         autoridad: new FormControl('', Validators.required)
-      })
+      }, {validators: confirmPasswordValidator})
     })
   }
 
